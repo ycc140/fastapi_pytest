5effd9d42153017906f5121fa197aeb0510c6a42
Anders Wiklund <126193526+ycc140@users.noreply.github.com>
2024-04-23 17:55:19 +0200

    Removed "dead meat" in the test data.

--- app/sms_document/documentation.py
+++ app/sms_document/documentation.py
@@ -6,15 +6,13 @@ License: Apache 2.0
 VERSION INFO:
     $Repo: fastapi_pytest
   $Author: Anders Wiklund
-    $Date: 2024-04-22 16:14:44
-     $Rev: 1
+    $Date: 2024-04-23 17:55:15
+     $Rev: 5
 ```
 """
 
 payload_documentation = {
     "UBID": "2a168739-b204-4abf-aec1-a88069e3cd08",
-    "fileName": "2a168739-b204-4abf-aec1-a88069e3cd08.zip",
-    "origName": "20211119-165542309564-01.xml",
     "documents": {
         "1000019152": {
             "smsData": {
